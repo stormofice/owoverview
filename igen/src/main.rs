86cb5d14d2602775699ed1f7a6d4d5eb63505429
stormofice <58337328+stormofice@users.noreply.github.com>
2025-04-10 19:08:08 +0200

    feat: Image to png

--- igen/src/main.rs
+++ igen/src/main.rs
@@ -1,4 +1,4 @@
-use image::{GenericImageView, Pixel};
+use image::{GenericImageView, Luma, Pixel};
 use std::io::Write;
 
 const EPD_WIDTH: usize = 800;
@@ -13,6 +13,7 @@ fn main() {
     image.load_image("glad.webp");
 
     image.to_file("output.bin");
+    image.to_img_file("output.png");
 }
 
 struct EpdImage {
@@ -38,9 +39,34 @@ impl EpdImage {
         }
     }
 
+    pub fn get_pixel(&self, x: usize, y: usize) -> u8 {
+        let byte_index = (y * EPD_WIDTH + x) / 8;
+        let bit_index = x % 8;
+        let clr = (self.data[byte_index] >> (7 - bit_index)) & 0x1;
+        if clr == 1 { WHITE } else { BLACK }
+    }
+
     pub fn to_file(&self, filename: &str) {
         let mut file = std::fs::File::create(filename).unwrap();
         file.write_all(&self.data).unwrap();
+    }
+
+    pub fn to_img_file(&self, filename: &str) {
+        let mut image = image::GrayImage::new(EPD_WIDTH as u32, EPD_HEIGHT as u32);
+        for y in 0..EPD_HEIGHT {
+            for x in 0..EPD_WIDTH {
+                image.put_pixel(
+                    x as u32,
+                    y as u32,
+                    if self.get_pixel(x, y) == WHITE {
+                        Luma([0xFF])
+                    } else {
+                        Luma([0x00])
+                    },
+                );
+            }
+        }
+        image.save(filename).expect("Could not save image")
     }
 
     pub fn load_image(&mut self, filename: &str) {
